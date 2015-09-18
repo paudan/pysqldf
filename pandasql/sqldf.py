f0a62b612a3c904ce5f4e5090c52f5f2864fb0fb
airtoxin <airtoxin@icloud.com>
2015-09-18 09:32:09 +0900

    Classify

--- pandasql/sqldf.py
+++ pandasql/sqldf.py
@@ -6,135 +6,140 @@ import re
 import os
 import inspect
 
-def _ensure_data_frame(obj, name):
-    """
-    obj a python object to be converted to a DataFrame
 
-    take an object and make sure that it's a pandas data frame
-    """
-    # we accept pandas Dataframe, and also dictionaries, lists, tuples
-    # we'll just convert them to Pandas Dataframe
-    if isinstance(obj, pd.DataFrame):
-        df = obj
-    elif isinstance(obj, (tuple, list)) :
-        #tuple and list case
-        if len(obj)==0:
-            return pd.Dataframe()
-
-        firstrow = obj[0]
-
-        if isinstance(firstrow, (tuple, list)):
-            #multiple-columns
-            colnames = ["c%d" % i for i in range(len(firstrow))]
-            df = pd.DataFrame(obj, columns=colnames)
+class SQLDF(object):
+    def __init__(self, env, inmemory=True, udfs={}, udafs={}):
+        super(SQLDF, self).__init__()
+        self.env = env
+        self.inmemory = inmemory
+        self.udfs = udfs
+        self.udafs = udafs
+        if self.inmemory:
+            self._dbname = ":memory:"
         else:
-            #mono-column
-            df = pd.DataFrame(obj, columns=["c0"])
+            self._dbname = ".pandasql.db"
+        self.conn = sqlite.connect(self._dbname, detect_types=sqlite.PARSE_DECLTYPES|sqlite.PARSE_COLNAMES)
+        self._set_udf(udfs)
+        self._set_udaf(udafs)
 
-    if not isinstance(df, pd.DataFrame) :
-        raise Exception("%s is not a Dataframe, tuple, list, nor dictionary" % name)
+    def __del__(self):
+        self.conn.close()
+        if not self.inmemory:
+            os.remove(self._dbname)
 
-    for col in df:
-        if df[col].dtype==np.int64:
-            df[col] = df[col].astype(np.float)
-        elif isinstance(df[col].get(0), pd.tslib.Timestamp):
-            df[col] = df[col].apply(lambda x: str(x))
+    def execute(self, query):
+        tables = self._extract_table_names(query)
+        for table in tables:
+            if table not in self.env:
+                self.conn.close()
+                if not self.inmemory:
+                    os.remove(self._dbname)
+                raise Exception("%s not found" % table)
+            df = self.env[table]
+            df = self._ensure_data_frame(df, table)
+            self._write_table(table, df)
 
-    return df
+        try:
+            result = read_sql(query, self.conn, index_col=None)
+            if 'index' in result:
+                del result['index']
+        except Exception:
+            result = None
+        finally:
+            self._del_table(tables)
+        return result
 
-def _extract_table_names(q):
-    "extracts table names from a sql query"
-    # a good old fashioned regex. turns out this worked better than actually parsing the code
-    rgx = '(?:FROM|JOIN)\s+([A-Za-z0-9_]+)'
-    tables = re.findall(rgx, q, re.IGNORECASE)
-    return list(set(tables))
+    def _extract_table_names(self, query):
+        "extracts table names from a sql query"
+        # a good old fashioned regex. turns out this worked better than actually parsing the code
+        rgx = '(?:FROM|JOIN)\s+([A-Za-z0-9_]+)'
+        tables = re.findall(rgx, query, re.IGNORECASE)
+        return list(set(tables))
 
-def _write_table(tablename, df, conn):
-    "writes a dataframe to the sqlite database"
+    def _ensure_data_frame(self, obj, name):
+        """
+        obj a python object to be converted to a DataFrame
 
-    for col in df.columns:
-        if re.search("[()]", col):
-            msg = "please follow SQLite column naming conventions: "
-            msg += "http://www.sqlite.org/lang_keywords.html"
-            raise Exception(msg)
+        take an object and make sure that it's a pandas data frame
+        """
+        # we accept pandas Dataframe, and also dictionaries, lists, tuples
+        # we'll just convert them to Pandas Dataframe
+        if isinstance(obj, pd.DataFrame):
+            df = obj
+        elif isinstance(obj, (tuple, list)) :
+            #tuple and list case
+            if len(obj)==0:
+                return pd.Dataframe()
 
-    to_sql(df, name=tablename, con=conn, flavor='sqlite')
+            firstrow = obj[0]
+
+            if isinstance(firstrow, (tuple, list)):
+                #multiple-columns
+                colnames = ["c%d" % i for i in range(len(firstrow))]
+                df = pd.DataFrame(obj, columns=colnames)
+            else:
+                #mono-column
+                df = pd.DataFrame(obj, columns=["c0"])
+
+        if not isinstance(df, pd.DataFrame) :
+            raise Exception("%s is not a Dataframe, tuple, list, nor dictionary" % name)
+
+        for col in df:
+            if df[col].dtype==np.int64:
+                df[col] = df[col].astype(np.float)
+            elif isinstance(df[col].get(0), pd.tslib.Timestamp):
+                df[col] = df[col].apply(lambda x: str(x))
+
+        return df
+
+    def _write_table(self, tablename, df):
+        "writes a dataframe to the sqlite database"
+
+        for col in df.columns:
+            if re.search("[()]", col):
+                msg = "please follow SQLite column naming conventions: "
+                msg += "http://www.sqlite.org/lang_keywords.html"
+                raise Exception(msg)
+
+        to_sql(df, name=tablename, con=self.conn, flavor='sqlite')
+
+    def _del_table(self, tablenames):
+        for tablename in tablenames:
+            self.conn.execute("drop table " + tablename)
+        self.conn.commit()
+
+    def _set_udf(self, udfs):
+        for name, func in udfs.items():
+            num_params = len(inspect.getargspec(func).args)
+            self.conn.create_function(name, num_params, func)
+
+    def _set_udaf(self, udafs):
+        for name, agg_class in udafs.items():
+            num_params = len(inspect.getargspec(agg_class.step).args) - 1 # subtract self
+            self.conn.create_aggregate(name, num_params, agg_class)
 
 
-def sqldf(q, env, inmemory=True, udfs={}, udafs={}):
-    """
-    query pandas data frames using sql syntax
+if __name__ == '__main__':
+    from pandas import DataFrame
 
-    Parameters
-    ----------
-    q: string
-        a sql query using DataFrames as tables
-    env: locals() or globals()
-        variable environment; locals() or globals() in your function
-        allows sqldf to access the variables in your python environment
-    inmemory: bool
-        memory/disk; default is in memory; if not memory then it will be
-        temporarily persisted to disk
-    udfs: dictionary of functions
-        user defined functions
-    udafs: dictionary of aggregation classes
-        user defined aggregate functions
+    df = DataFrame([
+        ["hoge", 0,  1,  2,  3,  4],
+        ["fuga", 5,  6,  7,  8,  9],
+        ["hoge", 10, 11, 12, 13, 14],
+        ["fuga", 15, 16, 17, 18, 19],
+        ["hoge", 20, 21, 22, 23, 24]
+    ], columns=["label", "a", "b", "c", "d", "e"])
+    class mysum(object):
+        def __init__(self):
+            self.count = 0
+        def step(self, val):
+            self.count += val
+        def finalize(self):
+            return self.count
 
-    Returns
-    -------
-    result: DataFrame
-        returns a DataFrame with your query's result
-
-    Examples
-    --------
-    >>> import pandas as pd
-    >>> df = pd.DataFrame({
-        "x": range(100),
-        "y": range(100)
-    })
-    >>> from pandasql import sqldf
-    >>> sqldf("select * from df;", globals())
-    >>> sqldf("select * from df;", locals())
-    >>> sqldf("select avg(x) from df;", locals())
-    """
-
-    if inmemory:
-        dbname = ":memory:"
-    else:
-        dbname = ".pandasql.db"
-    # conn = sqlite.connect(dbname, detect_types=sqlite.PARSE_DECLTYPES)
-    conn = sqlite.connect(dbname, detect_types=sqlite.PARSE_DECLTYPES|sqlite.PARSE_COLNAMES)
-    _set_udf(conn, udfs)
-    _set_udaf(conn, udafs)
-    tables = _extract_table_names(q)
-    for table in tables:
-        if table not in env:
-            conn.close()
-            if not inmemory :
-                os.remove(dbname)
-            raise Exception("%s not found" % table)
-        df = env[table]
-        df = _ensure_data_frame(df, table)
-        _write_table(table, df, conn)
-
-    try:
-        result = read_sql(q, conn, index_col=None)
-        if 'index' in result:
-            del result['index']
-    except Exception:
-        result = None
-    finally:
-        conn.close()
-        if not inmemory:
-            os.remove(dbname)
-    return result
-
-def _set_udf(conn, udfs):
-    for name, func in udfs.items():
-        num_params = len(inspect.getargspec(func).args)
-        conn.create_function(name, num_params, func)
-
-def _set_udaf(conn, udafs):
-    for name, agg_class in udafs.items():
-        num_params = len(inspect.getargspec(agg_class.step).args) - 1 # subtract self
-        conn.create_aggregate(name, num_params, agg_class)
+    sqldf = SQLDF(locals(), udfs={"half": lambda x: x / 2}, udafs={"mysum": mysum})
+    print(sqldf.execute("select * from df;"))
+    print(sqldf.execute("select sum(a) from df;"))
+    print(sqldf.execute("select half(a) from df;"))
+    print(sqldf.execute("select mysum(b) from df;"))
+    print(sqldf.execute("select MySum(b) from df;"))
