972338b68b36c9f8eec6db75eb62a2467de7ec80
airtoxin <airtoxin@icloud.com>
2015-09-21 22:41:06 +0900

    Add load_scripts tests

--- pysqldf/tests/tests.py
+++ pysqldf/tests/tests.py
@@ -3,7 +3,7 @@
 
 
 import pandas as pd
-from pysqldf import SQLDF, load_meat, load_births
+from pysqldf import SQLDF, load_iris, load_meat, load_births
 import string
 import unittest
 import os
@@ -13,6 +13,21 @@ class SQLDFTest(unittest.TestCase):
 
     def setUp(self):
         return
+
+    def test_load_iris(self):
+        iris = load_iris()
+        self.assertEqual(len(iris), 150)
+        self.assertEqual(list(iris.columns), ["sepal_length","sepal_width","petal_length","petal_width","species"])
+
+    def test_load_meat(self):
+        meat = load_meat()
+        self.assertEqual(len(meat), 827)
+        self.assertEqual(list(meat.columns), ["date","beef","veal","pork","lamb_and_mutton","broilers","other_chicken","turkey"])
+
+    def test_load_births(self):
+        births = load_births()
+        self.assertEqual(len(births), 408)
+        self.assertEqual(list(births.columns), ["date","births"])
 
     def test_select(self):
         df = pd.DataFrame({
