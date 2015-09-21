03b9cc6ad6d9d99d57cb3a76faa13331b23a01ae
airtoxin <airtoxin@icloud.com>
2015-09-21 19:21:45 +0900

    Fix setup script

--- release.sh
+++ release.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 pandoc -f markdown -t rst README.md > README.rst
-python setup.py install sdist $1
+python setup.py sdist # check egg and dist files
+
+# test pypi upload
+python setup.py register -r https://testpypi.python.org/pypi
+python setup.py sdist upload -r https://testpypi.python.org/pypi
+
+# install from test pypi
+# pip install --index-url https://testpypi.python.org/simple/ pysqldf
+
+# pypi upload
+# python setup.py register
+# python setup.py sdist upload
