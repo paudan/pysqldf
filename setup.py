fda7c7cfc06a925e9b216bc149c97c60a052916b
airtoxin <airtoxin@icloud.com>
2015-09-23 19:46:39 +0900

    version up

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="pysqldf",
-    version="1.1.0",
+    version="1.2.0",
     author="Ryoji Ishii",
     author_email="airtoxin@icloud.com",
     url="https://github.com/airtoxin/pysqldf/",
