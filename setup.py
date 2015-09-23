2c4d1d1b7cde282de534820ccd67d8cf46c59e00
airtoxin <airtoxin@icloud.com>
2015-09-23 19:46:06 +0900

    Update setup script

--- setup.py
+++ setup.py
@@ -20,6 +20,20 @@ setup(
     tests_require=[
         "pandas",
         "nose"
+    ],
+    keywords="sqldf pandas dataframe sql pandasql",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: SQL",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
     ]
 )
 
