72d380c7e9e477b6874b162ced6596d27525d5ce
airtoxin <airtoxin@icloud.com>
2015-10-04 19:12:14 +0900

    autopep8

--- pysqldf/load.py
+++ pysqldf/load.py
@@ -7,18 +7,22 @@ import pandas as pd
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
+
 def get_data(path):
     return os.path.join(ROOT, 'data', path)
+
 
 def load_iris():
     filename = get_data("iris.csv")
     df = pd.read_csv(filename)
     return df
 
+
 def load_meat():
     filename = get_data("meat.csv")
     df = pd.read_csv(filename, parse_dates=[0])
     return df
+
 
 def load_births():
     filename = get_data("births_by_month.csv")
