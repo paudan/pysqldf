41daa478700fcc6c1ff47e2aa092ae65782006e6
airtoxin <airtoxin@icloud.com>
2015-10-05 08:32:39 +0900

    tests autopep8

--- tests/load.py
+++ tests/load.py
@@ -13,20 +13,32 @@ class LoadTest(unittest.TestCase):
 
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
 
     def test_load_iris(self):
         iris = load_iris()
         self.assertEqual(len(iris), 150)
-        self.assertEqual(list(iris.columns), ["sepal_length","sepal_width","petal_length","petal_width","species"])
+        self.assertEqual(
+            list(
+                iris.columns), [
+                "sepal_length", "sepal_width", "petal_length", "petal_width", "species"])
 
     def test_load_meat(self):
         meat = load_meat()
         self.assertEqual(len(meat), 827)
-        self.assertEqual(list(meat.columns), ["date","beef","veal","pork","lamb_and_mutton","broilers","other_chicken","turkey"])
+        self.assertEqual(list(meat.columns),
+                         ["date",
+                          "beef",
+                          "veal",
+                          "pork",
+                          "lamb_and_mutton",
+                          "broilers",
+                          "other_chicken",
+                          "turkey"])
 
     def test_load_births(self):
         births = load_births()
         self.assertEqual(len(births), 408)
-        self.assertEqual(list(births.columns), ["date","births"])
+        self.assertEqual(list(births.columns), ["date", "births"])
