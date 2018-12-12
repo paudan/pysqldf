7ad683b5af5f52a4e0e26ef6a859dfe0464f104a
paulius <danpaulius@gmail.com>
2018-12-13 00:56:24 +0200

    Update requirements to be compatible with newer Python versions

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 import ast
 import re
+import sys
 from setuptools import setup
 
 with open("pysqldf/__init__.py", "rb") as f:
@@ -10,6 +11,9 @@ with open("pysqldf/__init__.py", "rb") as f:
         r"__version__\s+=\s+(.*)",
         f.read().decode("utf-8")).group(1)
     ))
+
+py_ver = sys.version_info
+old_python = (py_ver.major == 2 and py_ver.minor == 6) or (py_ver.major == 3 and py_ver.minor == 3)
 
 setup(
     name="pysqldf",
@@ -23,9 +27,7 @@ setup(
     package_data={"pysqldf": ["data/*.csv"]},
     description="sqldf for pandas",
     long_description=open("README.rst").read(),
-    install_requires=[
-        "pandas"
-    ],
+    install_requires=["pandas<=0.17"] if old_python else ["pandas"],
     tests_require=[
         "nose"
     ],
@@ -38,6 +40,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: SQL",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities"
